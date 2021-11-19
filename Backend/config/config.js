f4675ba64097e1ab0adcb16a67f76690d482b17d
DatskoRoman <pbk.datsko@gmail.com>
2021-11-19 06:58:07 +0200

    Initial commit

---
+++ Backend/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/fullstack-task',
+    PORT: process.env.PORT || '5000',
+
+    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL || 'romandatsko89@gmail.com',
+    NO_REPLY_PASSWORD: process.env.NO_REPLY_PASSWORD || 'qwerty',
+};
+
+
---
+++ Backend/config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    config: require('./config'),
+    emailTemplatesEnum: require('./email-templates-enum'),
+    regExp: require('./regexp'),
+    userTypeEnum: require('./user-type.enum'),
+
+};
